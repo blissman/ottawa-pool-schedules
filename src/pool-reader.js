a56f63d9de203495e224305a8dbb0c56ab837011
Bliss Man <blissman@gmail.com>
2023-03-21 14:43:06 -0400

    WIP oops

--- src/pool-reader.js
+++ src/pool-reader.js
@@ -1,61 +1,174 @@
-const pools = [
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/aquaview-community-hall",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/bearbrook-outdoor-pool",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/beaverbrook-outdoor-pool-kanata",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/bob-macquarrie-recreation-complex-orleans",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/brewer-pool-and-arena",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/canterbury-recreation-complex",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/cardelrec-recreation-complex-goulbourn",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/champagne-fitness-centre",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/corkstown-outdoor-pool",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/crestview-outdoor-pool",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/deborah-anne-kirwan-pool",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/dovercourt-recreation-centre",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/entrance-outdoor-pool",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/francois-dupuis-recreation-centre",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/general-burns-outdoor-pool",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/genest-outdoor-pool",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/glen-cairn-outdoor-pool",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/jack-purcell-community-centre",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/kanata-leisure-centre-and-wave-pool",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/katimavik-outdoor-pool",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/lowertown-pool",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/minto-recreation-complex-barrhaven",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/nepean-sportsplex",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/pinecrest-recreation-complex",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/plant-recreation-centre",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/ray-friel-recreation-complex",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/richcraft-recreation-complex-kanata",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/sawmill-creek-community-centre-and-pool",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/splash-wave-pool",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/st-laurent-complex",
-    "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/walter-baker-sports-centre"
-];
+let pools = {
+    "Aquaview Community Hall": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/aquaview-community-hall",
+        "address": "318 Aquaview Drive\\nOttawa ON K4A 0B5\\nCanada"
+    },
+    "Bearbrook Outdoor Pool": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/bearbrook-outdoor-pool",
+        "address": "2679 Innes Road\\nOttawa ON K1B 3J7\\nCanada"
+    },
+    "Beaverbrook Outdoor Pool - Kanata": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/beaverbrook-outdoor-pool-kanata",
+        "address": "1002 Beaverbrook Lane\\nOttawa ON K2K 1L1\\nCanada"
+    },
+    "Bob MacQuarrie Recreation Complex - Orléans": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/bob-macquarrie-recreation-complex-orleans",
+        "address": "1490 Youville Drive\\nOttawa ON K1C 2X8\\nCanada"
+    },
+    "Brewer Pool and Arena": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/brewer-pool-and-arena",
+        "address": "100 Brewer Way\\nOttawa ON K1S 5T1\\nCanada"
+    },
+    "Canterbury Recreation Complex": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/canterbury-recreation-complex",
+        "address": "2185 Arch Street\\nOttawa ON K1G 2H5\\nCanada"
+    },
+    "CARDELREC Recreation Complex Goulbourn": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/cardelrec-recreation-complex-goulbourn",
+        "address": "1500 Shea Road\\nOttawa ON K2S 0B2\\nCanada"
+    },
+    "Champagne Fitness Centre": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/champagne-fitness-centre",
+        "address": "321 King Edward Avenue\\nOttawa ON K1N 7M5\\nCanada"
+    },
+    "Corkstown Outdoor Pool": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/corkstown-outdoor-pool",
+        "address": "61 Corkstown Road\\nOttawa ON K3H 5B3\\nCanada"
+    },
+    "Crestview Outdoor Pool": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/crestview-outdoor-pool",
+        "address": "56 Fieldrow Street\\nOttawa ON K2G 2Y7\\nCanada"
+    },
+    "Deborah Anne Kirwan Pool": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/deborah-anne-kirwan-pool",
+        "address": "1300 Kitchener Avenue\\nOttawa ON K1V 6W2\\nCanada"
+    },
+    "Dovercourt Recreation Centre": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/dovercourt-recreation-centre",
+        "address": "411 Dovercourt Avenue\\nOttawa ON K2A 0S9\\nCanada"
+    },
+    "Entrance Outdoor Pool": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/entrance-outdoor-pool",
+        "address": "2 Eaton Street\\nOttawa ON K2H 9P1\\nCanada"
+    },
+    "François Dupuis Recreation Centre": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/francois-dupuis-recreation-centre",
+        "address": "2263 Portobello Boulevard\\nOttawa ON K4A 0X3\\nCanada"
+    },
+    "General Burns Outdoor Pool": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/general-burns-outdoor-pool",
+        "address": "107 Chesterton Drive\\nOttawa ON K2E 5T6\\nCanada"
+    },
+    "Genest Outdoor Pool": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/genest-outdoor-pool",
+        "address": "43 Ste-Cécile Street\\nOttawa ON K1L 5L7\\nCanada"
+    },
+    "Glen Cairn Outdoor Pool": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/glen-cairn-outdoor-pool",
+        "address": "70 Castlefrank Road\\nOttawa ON K2L 2N5\\nCanada"
+    },
+    "Jack Purcell Community Centre": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/jack-purcell-community-centre",
+        "address": "320 Jack Purcell Lane\\nOttawa ON K2P 2J5\\nCanada"
+    },
+    "Kanata Leisure Centre and Wave Pool": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/kanata-leisure-centre-and-wave-pool",
+        "address": "70 Aird Place\\nOttawa ON K2L 4C9\\nCanada"
+    },
+    "Katimavik Outdoor Pool": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/katimavik-outdoor-pool",
+        "address": "64 Chimo Drive\\nOttawa ON K2L 1Y9\\nCanada"
+    },
+    "Lowertown Pool": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/lowertown-pool",
+        "address": "40 Cobourg Street\\nOttawa ON K1N 8Z6\\nCanada"
+    },
+    "Minto Recreation Complex - Barrhaven": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/minto-recreation-complex-barrhaven",
+        "address": "3500 Cambrian Road\\nOttawa ON K2J 0V1\\nCanada"
+    },
+    "Nepean Sportsplex": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/nepean-sportsplex",
+        "address": "1701 Woodroffe Avenue\\nOttawa ON K2G 1W2\\nCanada"
+    },
+    "Pinecrest Recreation Complex": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/pinecrest-recreation-complex",
+        "address": "2250 Torquay Avenue\\nOttawa ON K2C 1J3\\nCanada"
+    },
+    "Plant Recreation Centre": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/plant-recreation-centre",
+        "address": "930 Somerset Street West\\nOttawa ON K1R 6R9\\nCanada"
+    },
+    "Ray Friel Recreation Complex": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/ray-friel-recreation-complex",
+        "address": "1585 Tenth Line Road\\nOttawa ON K1E 3E8\\nCanada"
+    },
+    "Richcraft Recreation Complex-Kanata": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/richcraft-recreation-complex-kanata",
+        "address": "4101 Innovation Drive\\nOttawa ON K2K 0J3\\nCanada"
+    },
+    "Sawmill Creek Community Centre and Pool": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/sawmill-creek-community-centre-and-pool",
+        "address": "3380 D’Aoust Avenue\\nOttawa ON K1T 1R5\\nCanada"
+    },
+    "Splash Wave Pool": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/splash-wave-pool",
+        "address": "2040 Ogilvie Road\\nOttawa ON K1J 7N8\\nCanada"
+    },
+    "St-Laurent Complex": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/st-laurent-complex",
+        "address": "525 Côté Street\\nOttawa ON K1K 0Z8\\nCanada"
+    },
+    "Walter Baker Sports Centre": {
+        "href": "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/facility-listing/walter-baker-sports-centre",
+        "address": "100 Malvern Drive\\nOttawa ON K2J 2G5\\nCanada"
+    }
+};
 
 const parser = new DOMParser();
-const pages = [];
 const tables = [];
 
-pools.forEach((uri) => {
-    fetch(uri)
+const appendSchedules = (elements) => {
+    const header = document.getElementsByTagName('h1')[0];
+    elements.forEach((element) => {
+        header.after(element);
+    });
+};
+
+const poolKeys = Object.keys(pools);
+
+poolKeys.forEach((pool) => {
+    fetch(pools[pool].href)
         .then((response) => response.text())
         .then((data) => parser.parseFromString(data, "text/html"))
-        .then((output) => pages.push(output))
-});
+        .then((output) => pools[pool]['page'] = output);
+})
 
 const parsePages = () => {
-    pages.forEach((page) => {
-    const buttons = Array.from(page.getElementsByTagName("button"));
-    buttons.forEach((button) => {
-        const expected = /Drop-in schedule - swim and aquafit/g;
-        if (button.textContent.match(expected)) {
-            const grandParent = button.parentElement.parentElement;
-            try {
-                tables.push(grandParent.getElementsByTagName('table')[0].cloneNode(true));
-            } catch (error) {
-                console.warn(error);
+    for (pool in pools) {
+        const page = pools[pool]['page'];
+        const buttons = Array.from(page.getElementsByTagName("button"));
+        buttons.forEach((button) => {
+            const expected = /\bdrop-in\b.*\bswim.*\b/i;
+            if (button.textContent.match(expected)) {
+                try {
+                    const parent = button.parentElement.parentElement.parentElement;
+                    let table = parent.getElementsByTagName('table')[0].cloneNode(true);
+                    let caption = table.getElementsByTagName('caption')[0];
+                    const captionRegex = /.+?(?=swim)|^swim/i;
+                    const captionText = caption.innerHTML;
+                    const anchorTag = `<a href=${pools[pool]['href']}> ${pool}</a> - `;
+                    caption.innerHTML = captionText.replace(captionRegex, anchorTag);
+                    tables.push(table);
+                } catch (error) {
+                    console.warn(error);
+                }
             }
-        }
-    });
-});
+        });
+    }
 };
+
+window.setTimeout(() => {
+    parsePages();
+    appendSchedules(tables);
+}, 1000);
--- src/reference/ref.js
+++ src/reference/ref.js
@@ -1,9 +1,36 @@
-const indexes = [
+const indices = [
     "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/place-listing?place_facets%5B0%5D=place_type%3A4285&text=&page=0",
     "https://ottawa.ca/en/recreation-and-parks/recreation-facilities/place-listing?place_facets%5B0%5D=place_type%3A4285&text=&page=1"
-]
+];
 
-const array = Array.from(document.getElementsByClassName("views-field-title"));
-array.forEach((element) => {
-    console.log(element.childNodes[0].href);
+const parser = new DOMParser();
+const pages = [];
+const pools = {};
+
+indices.forEach((index) => {
+    fetch(index)
+        .then((response) => response.text())
+        .then((data) => parser.parseFromString(data, "text/html"))
+        .then((output) => pages.push(output))
+});
+
+pages.forEach((page) => {
+    try { 
+        const array = Array.from(page.getElementsByClassName("views-field-title"));
+        array.forEach((element) => {
+            if (element.childNodes[0].href) {
+                const href = element.childNodes[0].href;
+                const name = element.childNodes[0].text;
+                const address = element.parentElement.getElementsByClassName('address')[0].textContent;
+                const entry = {
+                    'href': href,
+                    'address': address
+                };
+
+                pools[name] = entry;
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    } 
 });
